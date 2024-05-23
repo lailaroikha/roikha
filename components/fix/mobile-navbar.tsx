707f25b80f41ab280252f1e2abf02292cba21d34
Laila Roikha <lailajnnh@gmail.com>
2024-05-23 14:13:07 +0700

    navbar mobile dan web

--- components/fix/mobile-navbar.tsx
+++ components/fix/mobile-navbar.tsx
@@ -1,22 +1,19 @@
-// mobile.tsx
-
+'use client'
 import React from 'react';
-import Image from 'next/image';
-import Logo from '@/public/logokhajourney.svg';
-import Link from 'next/link';
-import styles from '@/components/fix/style/thememobile.module.css'
-<style />
+import styles from '@/components/fix/style/thememobile.module.css';
+
 
 const MobileNavbar: React.FC = () => {
     return (
-        <nav className={styles.navbarMobile}>
+        <div className={styles.mobileNavbar}>
+           
             <ul className={styles.menuMobile}>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Product</a></li>
                 <li><a href="#">About Us</a></li>
                 <li><a href="#">Our Team</a></li>
             </ul>
-        </nav>
+        </div>
     );
 };
 
--- components/fix/navbar.tsx
+++ components/fix/navbar.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+'use client'
+
+// Navbar.tsx
+import React, { useState } from 'react';
+import styles from '@/components/fix/style/thememobile.module.css';
+import MobileNavbar from '@/components/fix/mobile-navbar';
+import Logo from '@/public/logokhajourney.svg'; // path to your logo image
 import Image from 'next/image';
-import Logo from '@/public/logokhajourney.svg';
-import MobileNavbar from './mobile-navbar';
 
 const Navbar: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav className="navbar">
-            <div className="logo">
-                <Image src={Logo} alt="logo" width={60} height={60} />
+        <header className={styles.header}>
+            <div className={styles.logoContainer}>
+                <a href="/">
+                    <Image src={Logo} alt="Logo" width={60} height={60} className={styles.logo} />
+                </a>
             </div>
-            <ul className="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Product</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Our Team</a></li>
-            </ul>
-            <MobileNavbar />
-        </nav>
+            
+            <nav className={styles.navbar}>
+                {/* Desktop navigation */}
+                <ul className={styles.menu}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Our Team</a></li>
+                </ul>
+
+                {/* Hamburger menu icon (visible only in mobile) */}
+                <div className={styles.toggleButton} onClick={toggleMenu}>
+                    ☰
+                </div>
+
+                {isMenuOpen && <MobileNavbar />}
+            </nav>
+        </header>
     );
 };
 
 export default Navbar;
+
+
